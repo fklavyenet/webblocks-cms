c87b758c59f54aa007b3fb5fd34b0e907c66e422
Fklavyenet <opensource@fklavyenet.invalid>
2026-04-28 18:10:54 +0200

    Release 1.4.6

--- app/Support/WebBlocks.php
+++ app/Support/WebBlocks.php
@@ -10,7 +10,7 @@ final class WebBlocks
 
     public const HANDLE = 'webblocks-cms';
 
-    public const VERSION = '1.4.5';
+    public const VERSION = '1.4.6';
 
     public static function name(): string
     {
