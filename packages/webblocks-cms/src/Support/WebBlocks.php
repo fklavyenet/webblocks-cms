56c10ca7c41722688830e60c7e6444b4c9f649da
Fklavyenet <opensource@fklavyenet.invalid>
2026-05-26 08:22:54 +0200

    Prepare v1.32.57 release

--- packages/webblocks-cms/src/Support/WebBlocks.php
+++ packages/webblocks-cms/src/Support/WebBlocks.php
@@ -10,7 +10,7 @@ final class WebBlocks
 
   public const HANDLE = 'webblocks-cms';
 
-  public const VERSION = '1.32.56';
+  public const VERSION = '1.32.57';
 
   public const UI_VERSION = 'v2.7.7';
 
