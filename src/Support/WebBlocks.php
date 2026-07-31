0c6cba94f8c986bf17b2d6eeb42d9b62e03b54c9
Fklavyenet <opensource@fklavyenet.invalid>
2026-07-31 22:03:04 +0200

    chore(release): webblocks-cms 1.47.0
    CHANGELOG.md, src/Support/WebBlocks.php (VERSION 1.46.13 -> 1.47.0),
    tests/Unit/CanonicalVersionTest.php.

--- src/Support/WebBlocks.php
+++ src/Support/WebBlocks.php
@@ -10,7 +10,7 @@ final class WebBlocks
 
   public const HANDLE = 'webblocks-cms';
 
-  public const VERSION = '1.46.13';
+  public const VERSION = '1.47.0';
 
   public const UI_VERSION = 'v2.16.3';
 
