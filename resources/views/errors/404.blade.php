3eb7aff7189032b500125b031b95898da2a2f363
Fklavyenet <opensource@fklavyenet.invalid>
2026-08-06 21:07:04 +0200

    Revert "fix(brand): favicon raster set matches the SVG so every tab shows the same icon"
    This reverts commit 1b89f43483d08ba17c7d9a211658c3b8888845e8.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -71,7 +71,7 @@
             <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('cms/brand/favicon-16x16.png') }}">
             <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('cms/brand/favicon-32x32.png') }}">
             <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('cms/brand/apple-touch-icon.png') }}">
-            <link rel="shortcut icon" href="{{ asset('cms/brand/favicon.ico') }}">
+            <link rel="shortcut icon" href="{{ asset('cms/brand/favicon-32x32.png') }}">
         @endif
         <link rel="stylesheet" href="{{ WebBlocks::uiCssUrl() }}">
         @if (is_file($notFoundPublicCssPath))
